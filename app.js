1e72bf80723520cdce4cfc88c96ee1f80cc0bf5f
Souvik <souvikroy1999ab@gmail.com>
2023-08-31 17:19:17 +0530

    db configuration

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const exphbs = require('express-handlebars');
+const morgan = require('morgan');
 
 //Load config
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
 
