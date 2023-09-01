4834f3a563b820e10f0a958434eb77f4164c685b
Souvik <souvikroy1999ab@gmail.com>
2023-09-01 11:29:12 +0530

    added stories in dashboard

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ensureGuest, ensureAuth } = require('../middleware/auth');
 const router = express.Router();
 
+const Story = require('../models/Story');
+
 //@desc Login/Landing page
 //@route GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -12,10 +14,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc dashboard
 //@route GET /
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard', {
-    name: req.user.firstName,
-  });
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render('error/500');
+  }
 });
 
 module.exports = router;
